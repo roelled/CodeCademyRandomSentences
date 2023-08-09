ee1de6d5a028f1d7f1609e9d1d34ca892a30e90a
Danel Rølle Ørum <dro@rema1000.dk>
2023-08-09 14:43:22 +0200

    Created DB and function that saves scentences and reads to and from DB.

--- script.js
+++ script.js
@@ -10,4 +10,49 @@ function createRandomWord(subject, verb, object){
 
 };
 
-console.log("hello");
+function pushSentenceToDb(sentence){
+
+    const sqlite3 = require('sqlite3').verbose();
+    
+    // opens the database connection
+    let db = new sqlite3.Database('sentences.db');
+    
+    // insert one row into the savedSentences table
+    db.run(`INSERT INTO savedSentences (sentence) VALUES ("${sentence}");`, function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+    });
+
+    // close the database connection
+    db.close();
+
+};
+
+function readSentencesFromDb(){
+
+    const sqlite3 = require('sqlite3').verbose();
+    
+    // opens the database connection
+    let db = new sqlite3.Database('sentences.db');
+
+    // sql querry which needs to be run
+    let sql = "SELECT * FROM savedSentences";
+
+    // runs the querry in "sql" and accepts "err" if an error happens / if no "err" the "rows" will be exportet from the db
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        rows.forEach((row) => {
+          console.log(row.sentence);
+        });
+      });
+
+    // close the database connection
+    db.close();
+
+};
+
+
+readSentencesFromDb();
