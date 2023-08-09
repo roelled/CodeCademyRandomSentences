ac0aca221b2cfafa3bdd6121368291f66289a2db
Danel Rølle Ørum <dro@rema1000.dk>
2023-08-09 13:49:05 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Arrays that contain the words the function will be able to pull from.
+const subjectWords = [];
+const verbWords = [];
+const objectWords = [];
+
+// Function that creates random words from the given arrays.
+function createRandomWord(subject, verb, object){
+
+    
+
+};
+
+console.log("hello");
